037972616de80d7caa168f86a2c75c17b182b2dd
胡青杨 <hqy841440305@gmail.com>
2017-06-24 11:11:56 +0800

    Building website

--- website/js/index.js
+++ website/js/index.js
@@ -1,19 +1,22 @@
+const [windowWidth, windowHeight] = [window.innerWidth, window.innerHeight];
+
 
 
 window.addEventListener('load', () => {
     const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setClearColor('red');
+    renderer.setSize(400, 300);
+    renderer.setClearColor('black');
     document.getElementsByTagName('body')[0].appendChild(renderer.domElement);
 
     const scene = new THREE.Scene();
-    const camera  = new THREE.PerspectiveCamera(45, 4/3, 1, 1000);
-    camera.position.set(0, 0, 5);
+    const camera  = new THREE.OrthographicCamera(-1, 1, 1.5, -1.5, 1, 10);
+    camera.position.set(4, -3, 5);
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
     scene.add(camera);
 
     const cube = new THREE.Mesh(
-        new THREE.CubeGeometry(1, 2, 3),
-        new THREE.MeshBasicMaterial({ color: 'blue' })
+        new THREE.CubeGeometry(1, 1, 1),
+        new THREE.MeshBasicMaterial({ color: 'blue' , wireframe: true})
     );
     scene.add(cube);
     renderer.render(scene, camera)
