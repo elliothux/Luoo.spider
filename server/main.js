d412aabb4aba37e42f78597309feed0326597703
胡青杨 <hqy841440305@gmail.com>
2017-06-28 17:35:50 +0800

    Building website

--- server/main.js
+++ server/main.js
@@ -2,7 +2,7 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const colors = require('colors');
 const db = require('./db');
-const config = () => require('./package.json').config;
+const config = () => require('./package.json');
 
 
 const [app, router] = [new Koa(), new Router()];
@@ -45,22 +45,42 @@ router.get('/latest/:type', ctx => {
 
 router.get('/download/:platform', ctx => {
     const platform = parseInt(ctx.params.platform);
-    const URL = `http://os3s219a3.bkt.clouddn.com/Luoo.qy-v${require('./package.json').update.fullVersion}.${['dmg', 'exe', 'zip'][platform]}`;
-    ctx.redirect(URL)
+    const URL = `http://os3s219a3.bkt.clouddn.com/Luoo.qy-v${config().update.mainVersion}.0.${['dmg', 'exe', 'zip'][platform]}`;
+    ctx.redirect(URL);
+    log(`/download/${ctx.params.platform}`, ctx.request.ip)
 });
+
 
 router.get('/update/:platform/:preVersion', ctx => {
-    const info = require('./package.json').update;
-    if (info.type === 'full') ctx.body = JSON.stringify({
-        type: 'full',
-        version: info.fullVersion,
-        URL: `http://os3s219a3.bkt.clouddn.com/Luoo.qy-v${info.fullVersion}.${['dmg', 'exe', 'zip'][parseInt(ctx.params.platform)]}`
-    });
+    const info = config().update;
+    const preVersion = ctx.params.preVersion.split('.');
+
+    if (parseInt(preVersion[0]) === info.mainVersion && parseInt(preVersion[1]) === info.updateVersion)
+        ctx.body = JSON.stringify({ type: 'none' });
+
+    else if (parseInt(preVersion[0]) !== info.mainVersion)
+        ctx.body = JSON.stringify({
+            type: 'full',
+            version: `${info.mainVersion}.0`,
+            url: `http://os3s219a3.bkt.clouddn.com/Luoo.qy-v${info.mainVersion}.0.${['dmg', 'exe', 'zip'][parseInt(ctx.params.platform)]}`,
+            desc: info.mainDesc
+        });
+
     else ctx.body = JSON.stringify({
-        type: info.type,
-        version: info.version,
-        URL: `http://os3s219a3.bkt.clouddn.com/update-v${info.version}.zip}`
-    });
+            type: info.type,
+            version: `${info.mainVersion}.${info.updateVersion}`,
+            url: `http://os3s219a3.bkt.clouddn.com/update-v${info.mainVersion}.${info.updateVersion}.zip`,
+            desc: info.updateDesc
+        });
+    log(`/update/${ctx.params.platform}/${ctx.params.preVersion}`, ctx.request.ip)
 });
 
-app.use(router.routes()).listen(config().port);
+
+app.use(router.routes()).listen(config().config.port);
+
+
+
+function log(api, ip) {
+    ip = ip.split(':')[3];
+    console.log(`Response api  ${api.red}  to  ${ip.yellow}  at  ${(new Date()).toLocaleString().green}`)
+}
