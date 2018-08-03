3c6607dcc5be2d26ba028a0ca77c25d319d0105c
HuQingyang <i@do.codes>
2018-08-03 18:19:59 +0800

    Feat: Crawl singles

--- Luoo/spiders/single_spider.py
+++ Luoo/spiders/single_spider.py
@@ -15,13 +15,26 @@ class SingleSpider(scrapy.Spider):
     def parse(self, response):
         pass
 
-    def extract_banner_single(self, response):
+    @staticmethod
+    def extract_banner_single(response):
         banner = response.css('div.musician-banner')
         single_name = response.css('div.meta > a.title::text')[0].extract()
         single_artist = response.css('div.meta > p.performer::text')[0].extract()
         [single_referrer, single_date] = response.css('div.meta > p.date::text')[0].extract().split('・')
         single_referrer = single_referrer[0].replace('推荐人：', '').strip()
-        pass
+        single_desc = banner.css('div.meta > p.remark > span')[1].css('::text')[0].extract()
 
-    def extract_singles(self, response):
-        pass
+    @staticmethod
+    def extract_singles(response):
+        return map(
+            SingleSpider.extract_single,
+            response.css('div.musician-list > div.item')
+        )
+
+    @staticmethod
+    def extract_single(single):
+        single_name = single.css('div.musician-wrapper > a.title::text')[0].extract()
+        single_artist = single.css('div.musician-wrapper > p.performer::text')[0].extract()
+        [single_referrer, single_date] = single.css('div.musician-wrapper > p.date::text')[0].extract().split('・')
+        single_referrer = single_referrer[0].replace('推荐人：', '').strip()
+        single_desc = single.css('div.musician-wrapper > p.remark > span')[1].css('::text')[0].extract()
