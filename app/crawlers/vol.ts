954134edc7fdf78dd1d1cdb050146dba890fe2c6
HuQingyang <i@do.codes>
2019-01-10 22:02:20 +0800

    feat: spider using nodejs

--- app/crawlers/vol.ts
+++ app/crawlers/vol.ts
@@ -1,11 +1,13 @@
 
+import * as R from 'ramda';
 import { requestHTMLDOM } from '../utils';
+
 
 async function getLastPage(): Promise<number> {
     const doc = await requestHTMLDOM('http://www.luoo.net/music');
     const navigator: Element[] = Array.from(doc.querySelectorAll('.paginator > .page'));
-    const last = navigator.pop();
-    return parseInt(last.innerHTML);
+    const last = R.last(navigator).innerHTML;
+    return parseInt(last);
 }
 
 async function init() {
